cc164edb29ba6874bac4b1d0ddfc5aabba926017
Zargess-mobile <costa@hotmail.dk>
2014-09-10 12:35:59 +0200

    Milestone 1 DONE!!!!!

--- HjortehandlerneApp/src/com/example/hjortehandlerneapp/MainActivity.java
+++ HjortehandlerneApp/src/com/example/hjortehandlerneapp/MainActivity.java
@@ -6,9 +6,7 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
-
 
 public class MainActivity extends ActionBarActivity {
 	private EditText textbox;
