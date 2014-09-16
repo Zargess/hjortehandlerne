834ab4b5fba19594962b6efb8bdd9ee0f3d31ed3
Zargess <haagh9@hotmail.com>
2014-09-16 23:30:07 +0200

    http rest server

---
+++ PervasiveServer/PervasiveServer/Models/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PervasiveServer.Models {
+    public class User {
+        public string Name { get; set; }
+        public double Langtittude { get; set; }
+        public double Longtitude { get; set; }
+        public int id { get; set; }
+    }
+}
