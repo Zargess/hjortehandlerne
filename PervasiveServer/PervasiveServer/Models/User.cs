c663822855885ac5b68a51a25804a404b8cf82fd
Zargess-mobile <costa@hotmail.dk>
2014-09-17 21:24:39 +0200

    Revert "useless server crap"
    This reverts commit 152c55e905dfd9434139ae11f1848bd5d83fab99.

--- PervasiveServer/PervasiveServer/Models/User.cs
+++ PervasiveServer/PervasiveServer/Models/User.cs
@@ -2,12 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-using PervasiveServer.Controllers;
 
 namespace PervasiveServer.Models {
     public class User {
         public string Name { get; set; }
-        public string Location { get; set; }
-        public int Id { get; set; }
+        public double Langtittude { get; set; }
+        public double Longtitude { get; set; }
+        public int id { get; set; }
     }
 }
