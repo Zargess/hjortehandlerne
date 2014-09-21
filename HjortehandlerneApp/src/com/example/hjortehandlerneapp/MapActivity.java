f04410ace3b9e23ffbe8b02d1c7e939faa9c8ce4
Zargess-mobile <costa@hotmail.dk>
2014-09-21 17:08:31 +0200

    Flere brugere
    Har prøvet at lave det sådan at alle brugere med en location på serveren
    bliver tilføjet på mappet. Har ikke testet det endnu.

--- HjortehandlerneApp/src/com/example/hjortehandlerneapp/MapActivity.java
+++ HjortehandlerneApp/src/com/example/hjortehandlerneapp/MapActivity.java
@@ -1,8 +1,7 @@
 package com.example.hjortehandlerneapp;
 
 import java.net.MalformedURLException;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 import android.app.Activity;
 import android.app.AlertDialog;
@@ -21,7 +20,7 @@ public class MapActivity extends Activity implements OnMapLoadedCallback {
 	private LatLng myLocation;
 	private Users user;
 	private MarkerOptions marker;
-	private List<MarkerOptions> otherUsers;
+	private HashMap<String, MarkerOptions> otherUsers;
 	private MobileServiceClient mService;
 	private MobileServiceTable<Users> mTable;
 
@@ -33,22 +32,35 @@ public class MapActivity extends Activity implements OnMapLoadedCallback {
 		map = ((MapFragment) getFragmentManager().findFragmentById(R.id.map))
 				.getMap();
 		map.setOnMapLoadedCallback(this);
-		Intent i = getIntent();
-		user = new Users();
-		user.setId(i.getStringExtra("id"));
-		user.setLocation(i.getStringExtra("location"));
-		user.setName(i.getStringExtra("name"));
-		user.setPassword(i.getStringExtra("password"));
-		otherUsers = new ArrayList<MarkerOptions>();
+		getUser();
+		otherUsers = new HashMap<String, MarkerOptions>();
 		try {
 			mService = new MobileServiceClient(
 					"https://pervasivehjorte.azure-mobile.net/",
 					"dcsHgpaDELtZnyJwPatgDKuGjlvPOH95", this);
 			mTable = mService.getTable(Users.class);
 		} catch (MalformedURLException e) {
-			createAndShowDialog("There was an error creating the Mobile Service. Verify the URL", "Error");
+			createAndShowDialog(
+					"There was an error creating the Mobile Service. Verify the URL",
+					"Error");
 			e.printStackTrace();
 		}
+	}
+
+	private void getUser() {
+		Intent i = getIntent();
+		final String id = i.getStringExtra("id");
+		mTable.where().field("id").eq(id).execute(new TableQueryCallback<Users>() {
+			@Override
+			public void onCompleted(List<Users> users, int count, Exception ex,
+					ServiceFilterResponse response) {
+				if (users.size() == 1) {
+					user = users.get(0);
+				} else {
+					createAndShowDialog("An error orcurred with the user id: " + id, "Error");
+				}
+			}
+		});
 	}
 
 	@Override
@@ -59,7 +71,8 @@ public class MapActivity extends Activity implements OnMapLoadedCallback {
 			map.setOnMyLocationChangeListener(new OnMyLocationChangeListener() {
 				@Override
 				public void onMyLocationChange(Location location) {
-					myLocation = new LatLng(location.getLatitude(), location.getLongitude());
+					myLocation = new LatLng(location.getLatitude(), location
+							.getLongitude());
 					if (marker == null) {
 						marker = new MarkerOptions().position(myLocation)
 								.title("My location").snippet(user.getName())
@@ -71,10 +84,41 @@ public class MapActivity extends Activity implements OnMapLoadedCallback {
 					updatePositionOnServer(myLocation);
 					CameraUpdate update = CameraUpdateFactory.newLatLngZoom(
 							myLocation, 16);
-					map.moveCamera(update);
+					map.animateCamera(update);
 				}
 			});
 		}
+	}
+
+	private void findOtherUsers() {
+		mTable = mService.getTable(Users.class);
+		mTable.where().field("id").ne(user.getId()).and().field("location").ne("").execute(new TableQueryCallback<Users>() {
+					@Override
+					public void onCompleted(List<Users> result, int count,
+							Exception ex, ServiceFilterResponse response) {
+						for (Users user : result) {
+							if (!otherUsers.containsKey(user.getId())) {
+								MarkerOptions marker = new MarkerOptions()
+										.position(stringToCoordinate(user.getLocation()))
+										.snippet(user.getName())
+										.draggable(false);
+								otherUsers.put(user.getId(), marker);
+								map.addMarker(marker);
+							} else {
+								MarkerOptions marker = otherUsers.get(user.getId());
+								marker.position(stringToCoordinate(user.getLocation()));
+							}
+						}
+					}
+				});
+	}
+
+	private LatLng stringToCoordinate(String s) {
+		String[] temp = s.split(",");
+		double lat = Double.parseDouble(temp[0]);
+		double lng = Double.parseDouble(temp[1]);
+
+		return new LatLng(lat, lng);
 	}
 
 	private void updatePositionOnServer(LatLng myLocation) {
@@ -84,14 +128,10 @@ public class MapActivity extends Activity implements OnMapLoadedCallback {
 		user.setLocation(pos);
 		mTable.update(user, new TableOperationCallback<Users>() {
 			@Override
-			public void onCompleted(Users arg0, Exception arg1,
-					ServiceFilterResponse arg2) {
-				// TODO Auto-generated method stub
-				
-			}
+			public void onCompleted(Users arg0, Exception arg1, ServiceFilterResponse arg2) {}
 		});
 	}
-	
+
 	private void createAndShowDialog(String message, String title) {
 		AlertDialog.Builder builder = new AlertDialog.Builder(this);
 
